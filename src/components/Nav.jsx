7ae42807a251ef051b3d056b24bd663dcf4b61b7
Pratikkk <kenipratik9@gmail.com>
2023-03-28 01:22:05 +0530

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav className="nav-wrapper">
+      <div className="nav-content">
+        <ul className="list-styled"></ul>
+      </div>
+    </nav>
+  );
+}
